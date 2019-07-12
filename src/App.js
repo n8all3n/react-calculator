bf5f5db28fff06347a2960f21ba4b5a2bdcfe723
n8all3n <n8all3n@gmail.com>
2019-07-12 13:51:40 -0600

    Updated code to calculate results

--- src/App.js
+++ src/App.js
@@ -2,52 +2,70 @@ import React from 'react';
 
 import './App.css';
 import CalculatorButton from './CalculatorButton'
-import calculate from './Calculate';
+import calc from './Calculate';
 
 export default class App extends React.Component {
   state = {
-    total: '0',
-    nextBtn: null
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+
+  onDigitClick = (btnType) => {
+    this.setState(calc.inputDigit(btnType, this.state));
   }
 
-  onBtnClick = (btnType) => {
-    this.setState(calculate(this.state, btnType));
+  allClear = () => {
+    this.setState(calc.resetCalculator(this.state));
+  }
+
+  inputDecimal = () => {
+    this.setState(calc.inputDecimal(this.state));
+  }
+
+  handleOperator = (nextOperator) => {
+    this.setState(calc.handleOperator(nextOperator, this.state));
+  }
+
+  handlePositiveNegative = () => {
+    this.setState(calc.handlePositiveNegative(this.state));
   }
 
   render() {
     return (
       <div className="app">
           <div className="result">
-            <span>{this.state.total}</span>
+            <span>{this.state.displayValue}</span>
           </div>
           <div className="button-row">
-            <CalculatorButton name="AC" buttonType="calc-button function-button" onClick={this.onBtnClick} />
-            <CalculatorButton name="+/-" buttonType="calc-button function-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="%" buttonType="calc-button function-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="÷" buttonType="calc-button orange-button" onClick={this.onBtnClick}/>
+            <CalculatorButton name="AC" buttonType="calc-button function-button" onClick={this.allClear} />
+            <CalculatorButton name="+/-" buttonType="calc-button function-button" onClick={this.handlePositiveNegative}/>
+            <CalculatorButton name="%" buttonType="calc-button function-button" onClick={() =>this.handleOperator('%')}/>
+            <CalculatorButton name="÷" buttonType="calc-button orange-button" onClick={() =>this.handleOperator('/')}/>
           </div>
           <div className="button-row">
-            <CalculatorButton name="7" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="8" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="9" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="X" buttonType="calc-button orange-button" onClick={this.onBtnClick}/>
+            <CalculatorButton name="7" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="8" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="9" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="X" buttonType="calc-button orange-button" onClick={() => this.handleOperator('*')}/>
           </div>
           <div className="button-row">
-            <CalculatorButton name="4" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="5" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="6" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="-" buttonType="calc-button orange-button"onClick={this.onBtnClick} />
+            <CalculatorButton name="4" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="5" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="6" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="-" buttonType="calc-button orange-button"onClick={() =>this.handleOperator('-')} />
           </div>
           <div className="button-row">
-            <CalculatorButton name="1" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="2" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="3" buttonType="calc-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="+" buttonType="calc-button orange-button" onClick={this.onBtnClick}/>
+            <CalculatorButton name="1" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="2" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="3" buttonType="calc-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="+" buttonType="calc-button orange-button" onClick={() =>this.handleOperator('+')}/>
           </div>
           <div className="button-row">
-            <CalculatorButton name="0" buttonType="calc-button wide-button" onClick={this.onBtnClick}/>
-            <CalculatorButton name="." buttonType="calc-button" onClick={this.onBtnClick} />
-            <CalculatorButton name="=" buttonType="calc-button orange-button" onClick={this.onBtnClick}/>
+            <CalculatorButton name="0" buttonType="calc-button wide-button" onClick={this.onDigitClick}/>
+            <CalculatorButton name="." buttonType="calc-button" onClick={this.inputDecimal} />
+            <CalculatorButton name="=" buttonType="calc-button orange-button" onClick={() =>this.handleOperator('=')}/>
           </div>
       </div>
     );
--- src/Calculate.js
+++ src/Calculate.js
@@ -1,25 +1,103 @@
-
-export default function calculate(currState, btnType) {
-    if (btnType === 'AC') {
-        return {
-            total: '0',
-            nextButton: null
-        };
-    }
-
-    
-    var res = parseInt(btnType, 10)
-
-    if (!isNaN(res)) {
-        if (currState.nextButton == null) {
-            if (currState.total === '0') {
-                currState.total = btnType;
-            }
-            else {
-                currState.total = currState.total + parseInt(btnType, 10);
-            }
+function inputDigit(digit, calculator) {
+    if (calculator.waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+        if (calculator.displayValue === '-0') {
+            calculator.displayValue = '-' + digit;
+        } else if (calculator.displayValue === '0') {
+            calculator.displayValue = digit;
+        } else {
+            calculator.displayValue = calculator.displayValue + digit;
         }
     }
 
-   return currState;
+    return calculator;
+}
+
+function resetCalculator(calculator) {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+
+    return calculator
+}
+
+function inputDecimal(calculator) {
+	if (calculator.waitingForSecondOperand === true) return;
+  
+  // If the `displayValue` does not contain a decimal point
+  if (!calculator.displayValue.includes('.')) {
+    // Append the decimal point
+    calculator.displayValue += '.';
+  }
+
+  return calculator;
+}
+
+function handleOperator(nextOperator, calculator) {
+    const performCalculation = {
+        '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+      
+        '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+      
+        '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+      
+        '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+        '%': (firstOperand, secondOperand) => firstOperand / 100,
+
+        '=': (firstOperand, secondOperand) => secondOperand
+      };
+
+
+    const inputValue = parseFloat(calculator.displayValue);
+  
+    if (nextOperator === '%') {
+        const result = performCalculation['%'](inputValue);
+        calculator.displayValue = String(result);
+        return calculator;
+    }
+
+    if (calculator.operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      return;
+    }
+  
+    if (calculator.firstOperand == null) {
+      calculator.firstOperand = inputValue;
+    } else if (calculator.operator) {
+      const currentValue = calculator.firstOperand || 0;
+      const result = performCalculation[calculator.operator](currentValue, inputValue);
+  
+      calculator.displayValue = String(result);
+      calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+
+    return calculator;
+  }
+  
+  
+  function handlePositiveNegative(calculator) {
+    if (calculator.waitingForSecondOperand === true) return;
+
+    if (calculator.displayValue === '0') {
+        calculator.displayValue = '-0';
+    } else {
+        calculator.displayValue = (-1 * parseFloat(calculator.displayValue)).toString();
+    }
+    return calculator;
+  }
+
+
+export default {
+    inputDigit: inputDigit,
+    resetCalculator: resetCalculator,
+    inputDecimal: inputDecimal,
+    handleOperator: handleOperator,
+    handlePositiveNegative: handlePositiveNegative
 }
